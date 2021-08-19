c9c3b571e2ded49708b758ba657364d68de940d5
serafim <serafim.gerasimov@mail.ru>
2021-08-19 15:35:15 +0300

    Updates

---
+++ component---src-pages-404-tsx-4c96edd244c6b291e9c0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{gqNJ:function(e,t,a){},"i6+/":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n);a("gqNJ");t.default=function(){return l.a.createElement("div",{className:"home"},l.a.createElement("title",null,"404"),l.a.createElement("div",{className:"page"},l.a.createElement("div",{className:"page-content"},l.a.createElement("h1",null,"404"),l.a.createElement("p",null,"Not found"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-4c96edd244c6b291e9c0.js.map
