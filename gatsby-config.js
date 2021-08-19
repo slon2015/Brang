ed626a6dd0a71c2c3045cf30e1eee8a64f9d0640
serafim <serafim.gerasimov@mail.ru>
2021-08-19 21:48:50 +0300

    MUI plugin for SSG

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,14 @@ const ymId = process.env.YM;
 /* eslint-disable */
 module.exports = {
   /* Your site config here */
+  pathPrefix: "/Brang",
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript",
     "gatsby-plugin-less",
     "gatsby-transformer-json",
     "gatsby-transformer-mdx",
+    "gatsby-plugin-material-ui",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
